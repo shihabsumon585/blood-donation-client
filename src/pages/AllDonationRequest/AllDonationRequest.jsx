91e5afb4cd902568b979509e458e2fa7dfad2136
Md Shihab Sumon <shihabsumon585@gmail.com>
2025-12-22 18:36:07 +0600

    complete project and waiting for checking important thinks

--- src/pages/AllDonationRequest/AllDonationRequest.jsx
+++ src/pages/AllDonationRequest/AllDonationRequest.jsx
@@ -1,9 +1,241 @@
-import React from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
+import useAxiosSecure from '../../hooks/useAxiosSecure/useAxiosSecure';
+import useAxios from '../../hooks/useAxios/useAxios';
+import { AuthContext } from '../../provider/AuthProvider';
 
 const AllDonationRequest = () => {
+
+    const axiosInstance = useAxios();
+    const { role } = useContext(AuthContext);
+
+    const [confirmId, setConfirmId] = useState(null);
+
+    const [myRequests, setMyRequests] = useState([]);
+    const [totalRequest, setTotalRequest] = useState(0);
+    const [itemsPerPage, setItemsPerPage] = useState(10);
+    const [currentPage, setCurrentPage] = useState(1);
+    const axiosSecure = useAxiosSecure();
+
+    console.log(myRequests);
+
+    const fetchingData = useCallback(() => {
+        axiosSecure.get(`/all-donation-request?page=${currentPage - 1}&size=${itemsPerPage}`)
+            .then(res => {
+                setMyRequests(res.data.request);
+                setTotalRequest(res.data.totalRequest);
+            })
+    }, [axiosSecure, currentPage, itemsPerPage])
+
+    useEffect(() => {
+        fetchingData();
+    }, [fetchingData])
+
+    const numberOfPages = Math.ceil(totalRequest / itemsPerPage);
+    const pages = [...Array(numberOfPages).keys()].map(e => e + 1);
+
+    const handlePrev = () => {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
+
+    const handleNext = () => {
+        if (currentPage < pages.length) {
+            setCurrentPage(currentPage + 1);
+        }
+    }
+
+
+    const handleStatusChange = (_id, updateStatus) => {
+
+        const status = updateStatus;
+
+        const updateData = { status };
+
+        axiosInstance.patch(`/update-status/${_id}`, updateData)
+            .then(res => {
+                console.log(res.data);
+                fetchingData();
+            })
+            .catch(err => {
+                console.log(err);
+            })
+        alert("clicked inprogress button...")
+    }
+
+    const handleDelete = (_id) => {
+        axiosInstance.delete(`/requests-delete/${_id}`)
+            .then(res => {
+                console.log(res.data);
+                fetchingData()
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    console.log(role);
+
+    // const handleViewDetails = 
+
     return (
         <div>
-            All Donation Request...
+            <div className="overflow-x-auto bg-white rounded-2xl shadow">
+                <table className="min-w-full border border-gray-200">
+                    <thead className="bg-gray-100 text-left text-sm font-semibold text-gray-700">
+                        <tr>
+                            <th className="px-4 py-3">Serial</th>
+                            <th className="px-4 py-3">Recipient</th>
+                            <th className="px-4 py-3">Location</th>
+                            <th className="px-4 py-3">Date</th>
+                            <th className="px-4 py-3">Time</th>
+                            <th className="px-4 py-3">Blood</th>
+                            <th className="px-4 py-3">Status</th>
+                            <th className="px-4 py-3">Donor Info</th>
+                            <th className="px-4 py-3 text-center">Actions</th>
+                        </tr>
+                    </thead>
+
+                    <tbody className="divide-y">
+                        {myRequests.map((d, index) => (
+                            <tr key={d._id} className="text-sm">
+                                <td className="px-4 py-3 font-medium">{(currentPage * 10) + (index + 1) - 10}</td>
+                                <td className="px-4 py-3 font-medium">{d.recipientName}</td>
+                                <td className="px-4 py-3">
+                                    {d.district}, {d.upazila}
+                                </td>
+                                <td className="px-4 py-3">{d.donationDate}</td>
+                                <td className="px-4 py-3">{d.donationTime}</td>
+                                <td className="px-4 py-3 font-semibold text-red-600">
+                                    {d.bloodGroup}
+                                </td>
+                                <td className="px-4 py-3">
+                                    <span
+                                        className={`px-2 py-1 rounded text-xs font-semibold ${d.status === "pending"
+                                            ? "bg-yellow-100 text-yellow-700"
+                                            : d.status === "inprogress"
+                                                ? "bg-blue-100 text-blue-700"
+                                                : d.status === "done"
+                                                    ? "bg-green-100 text-green-700"
+                                                    : "bg-red-100 text-red-700"
+                                            }`}
+                                    >
+                                        {d.status}
+                                    </span>
+
+
+                                    {d.status === "inprogress" && (
+                                        <div className="mt-2 flex gap-2">
+                                            <button
+                                                onClick={() => handleStatusChange(d._id, "done")}
+                                                className="px-2 py-1 text-xs rounded bg-green-600 text-white"
+                                            >
+                                                Done
+                                            </button>
+                                            <button
+                                                onClick={() => handleStatusChange(d._id, "canceled")}
+                                                className="px-2 py-1 text-xs rounded bg-red-600 text-white"
+                                            >
+                                                Cancel
+                                            </button>
+                                        </div>
+                                    )}
+                                </td>
+
+                                <td className="px-4 py-3">
+                                    {d.status === "inprogress" && d.donarEmail ? (
+                                        <div className="text-xs">
+                                            <p className="font-medium">{d?.donarName}</p>
+                                            <p className="text-gray-500">{d?.donarEmail}</p>
+                                        </div>
+                                    ) : (
+                                        <span className="text-gray-400">—</span>
+                                    )}
+                                    {
+
+                                    }
+                                </td>
+                                <td className="px-4 py-3">
+                                    <div className="flex justify-center gap-2">
+                                        <Link
+
+                                            to={`/view-details/${d._id}`}
+                                            className="px-3 py-1 text-xs rounded bg-gray-200"
+
+                                        >
+                                            View
+                                        </Link>
+                                        {
+                                            role !== "volunteer" && (
+                                                <div className='flex justify-center gap-2'>
+                                                    <Link
+
+                                                        to={`/dashbord/edit-donation/${d._id}`}
+                                                        className="px-3 py-1 text-xs rounded bg-blue-600 text-white"
+                                                    >
+                                                        Edit
+                                                    </Link>
+                                                    <button
+
+                                                        onClick={() => setConfirmId(d._id)}
+                                                        className="px-3 py-1 text-xs rounded bg-red-600 text-white"
+                                                    >
+                                                        Delete
+                                                    </button>
+                                                </div>
+                                            )
+                                        }
+                                    </div>
+
+                                    {confirmId === d._id && (
+                                        <div className="fixed inset-0 bg-black/40 flex items-center justify-center">
+                                            <div className="bg-white p-6 rounded-xl w-80">
+                                                <h3 className="font-semibold text-lg mb-3">
+                                                    Confirm Delete
+                                                </h3>
+                                                <p className="text-sm text-gray-600 mb-4">
+                                                    Are you sure you want to delete this donation request?
+                                                </p>
+                                                <div className="flex justify-end gap-3">
+                                                    <button
+                                                        onClick={() => setConfirmId(null)}
+                                                        className="px-4 py-2 rounded bg-gray-200"
+                                                    >
+                                                        Cancel
+                                                    </button>
+                                                    <button
+                                                        onClick={() => {
+                                                            handleDelete(d._id);
+                                                            setConfirmId(null);
+                                                        }}
+                                                        className="px-4 py-2 rounded bg-red-600 text-white"
+                                                    >
+                                                        Delete
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )}
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <div className='my-6 flex justify-center items-center gap-4'>
+                    <button onClick={handlePrev} className="btn">Prev</button>
+                    {
+                        pages.map((page, index) =>
+                            <button
+                                key={index}
+                                className={`btn hover:underline hover:text-blue-500 ${page === currentPage ? "bg-[#435585] text-white" : ""}`}
+                                onClick={() => setCurrentPage(page)}
+                            >{page}</button>
+                        )
+                    }
+                    <button onClick={handleNext} className="btn">Next</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/EditDonationRequest/EditDonationRequest.jsx
+++ src/pages/EditDonationRequest/EditDonationRequest.jsx
@@ -1,11 +1,37 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import useAxios from '../../hooks/useAxios/useAxios';
+import axios from 'axios';
+import useAxiosSecure from '../../hooks/useAxiosSecure/useAxiosSecure';
+import { AuthContext } from '../../provider/AuthProvider';
 
 const EditDonationRequest = () => {
     const { id } = useParams()
     const axiosInstance = useAxios();
     const [data, setData] = useState([]);
+
+    const { user } = useContext(AuthContext);
+    const [districts, setDistricts] = useState([]);
+    const [upazilas, setUpazilas] = useState([]);
+    const axiosSecure = useAxiosSecure();
+
+    useEffect(() => {
+        axios.get("/districts.json")
+            .then(res => {
+                setDistricts(res.data)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
+        axios.get("/upazilas.json")
+            .then(res => {
+                setUpazilas(res.data)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, [])
 
     const fetchingData = useCallback(() => {
         axiosInstance.get(`/view-details/${id}`)
@@ -15,33 +41,204 @@ const EditDonationRequest = () => {
             .catch(err => {
                 console.log(err);
             })
-    })
+    }, [axiosInstance, id])
     useEffect(() => {
         fetchingData();
     }, [fetchingData])
 
+    console.log(data);
+
     const handleUpdateData = (e) => {
         e.preventDefault();
 
-        
+        const form = e.target;
+        const message = form.message.value;
+        const donationTime = form.donationTime.value;
+        const donationDate = form.donationDate.value;
+        const bloodGroup = form.bloodGroup.value;
+        const address = form.address.value;
+        const hospitalName = form.hospitalName.value;
+        const district = form.district.value;
+        const upazila = form.upazila.value;
+        const recipientName = form.recipientName.value;
+        const requesterEmail = form.requesterEmail.value;
+        const requesterName = form.requesterName.value;
 
         const updateData = {
-
-        }
+            message,
+            donationTime,
+            donationDate,
+            bloodGroup,
+            address,
+            hospitalName,
+            district,
+            upazila,
+            recipientName,
+            requesterEmail,
+            requesterName
+        };
 
         axiosInstance.patch(`/edit-donation/${id}`, updateData)
-        .then(res => {
-            console.log(res.data);
-            fetchingData();
-        })
-        .catch(err => {
-            console.log(err);
-        })
+            .then(res => {
+                console.log(res.data);
+                fetchingData();
+            })
+            .catch(err => {
+                console.log(err);
+            })
     }
 
     return (
         <div>
-            <h1>Edit donation request page.. id: {id}</h1>
+            <div className='mt-2 mx-auto flex justify-center'>
+                <div className="min-w-3xl mx-auto p-6 bg-white shadow rounded-lg">
+                    <h2 className="text-2xl font-bold mb-6 text-center">
+                        Update Request
+                    </h2>
+
+                    <form onSubmit={handleUpdateData} className="space-y-4">
+
+                        {/* Requester Name */}
+                        <div>
+                            <label className="label">Requester Name</label>
+                            <input
+                                name='requesterName'
+                                type="text"
+                                value={user?.displayName}
+                                readOnly
+                                className="input input-bordered w-full bg-gray-100"
+                            />
+                        </div>
+
+                        {/* Requester Email */}
+                        <div>
+                            <label className="label">Requester Email</label>
+                            <input
+                                name='requesterEmail'
+                                type="email"
+                                value={user?.email || ""}
+                                readOnly
+                                className="input input-bordered w-full bg-gray-100"
+                            />
+                        </div>
+
+                        {/* Recipient Name */}
+                        <div>
+                            <label className="label">Recipient Name</label>
+                            <input
+                                type="text"
+                                name="recipientName"
+                                required
+                                className="input input-bordered w-full"
+                            />
+                        </div>
+
+                        {/* Select a the district */}
+                        <label className="label">Recipient District</label>
+                        <select name='district' defaultValue="Choose the District" className="select w-full">
+                            <option value="">Select your District</option>
+                            {
+                                districts.map(district => <option key={district.id} value={district.name}>{district.name}</option>)
+                            }
+                        </select>
+
+                        {/* Select a the upazila */}
+                        <label className="label">Recipient Upazila</label>
+                        <select name='upazila' defaultValue="Choose the Upazila" className="select w-full">
+                            <option value="">Select your Upazila</option>
+                            {
+                                upazilas.map(upazila => <option key={upazila.id} value={upazila.name}>{upazila.name}</option>)
+                            }
+                        </select>
+
+                        {/* Hospital Name */}
+                        <div>
+                            <label className="label">Hospital Name</label>
+                            <input
+                                type="text"
+                                name="hospitalName"
+                                required
+                                placeholder="Dhaka Medical College Hospital"
+                                className="input input-bordered w-full"
+                            />
+                        </div>
+
+                        {/* Full Address */}
+                        <div>
+                            <label className="label">Full Address</label>
+                            <input
+                                type="text"
+                                name="address"
+                                required
+                                placeholder="Zahir Raihan Rd, Dhaka"
+                                className="input input-bordered w-full"
+                            />
+                        </div>
+
+                        {/* Blood Group */}
+                        <div>
+                            <label className="label">Blood Group</label>
+                            <select
+                                name="bloodGroup"
+                                required
+                                className="select select-bordered w-full"
+                            >
+                                <option value="">Select Blood Group</option>
+                                <option>A+</option>
+                                <option>A-</option>
+                                <option>B+</option>
+                                <option>B-</option>
+                                <option>AB+</option>
+                                <option>AB-</option>
+                                <option>O+</option>
+                                <option>O-</option>
+                            </select>
+                        </div>
+
+                        {/* Donation Date */}
+                        <div>
+                            <label className="label">Donation Date</label>
+                            <input
+                                type="date"
+                                name="donationDate"
+                                required
+                                className="input input-bordered w-full"
+                            />
+                        </div>
+
+                        {/* Donation Time */}
+                        <div>
+                            <label className="label">Donation Time</label>
+                            <input
+                                type="time"
+                                name="donationTime"
+                                required
+                                className="input input-bordered w-full"
+                            />
+                        </div>
+
+                        {/* Request Message */}
+                        <div>
+                            <label className="label">Request Message</label>
+                            <textarea
+                                name="message"
+                                required
+                                rows="4"
+                                className="textarea textarea-bordered w-full"
+                                placeholder="Explain why blood is needed..."
+                            />
+                        </div>
+
+                        {/* Submit Button */}
+                        <button
+                            type="submit"
+                            className="btn btn-error w-full text-white"
+                        >
+                            Update Request
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
